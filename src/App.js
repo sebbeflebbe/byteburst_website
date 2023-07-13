24ae0ed48a40d4bccb72862887c66cadf06a593e
Sebastian-Andersson <sebastian@cshift.tech>
2023-07-13 22:31:42 +0200

    getting started with skeleton

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './byteburstlogo.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+      <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet"/>
+      </head>
+      <div className="header">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo-image" />
+        </div>
+        <div className="byteburst-container">
+          <p className="byteburst-text">
+            <strong>ByteBurst</strong>
+          </p>
+        </div>
+      </div>
+      <div className="board-container">
+        <div className="board blue-board">
+          <button className="button dark-blue-button">Vision</button>
+          <button className="button dark-blue-button">Our Projects</button>
+          <button className="button dark-blue-button">Our Consultants</button>
+          <button className="button dark-blue-button">Contact</button>
+        </div>
+        <div className="board dark-green-board"></div>
+      </div>
     </div>
   );
 }
