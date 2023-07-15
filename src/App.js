3b24bea5317c110165e98a282f3c1401ab42ea16
Sebastian-Andersson <sebastian@cshift.tech>
2023-07-15 09:46:52 +0200

    added animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import { Box, Button, ButtonGroup, Menu, MenuButton, MenuList, MenuItem, Image, extendTheme, ChakraProvider } from "@chakra-ui/react";
+import { useState } from 'react';
+import React from 'react';
+import Lottie from 'react-lottie';
+import 'animate.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Image,
+  extendTheme,
+  ChakraProvider,
+  Card,
+  CardHeader,
+  CardBody,
+} from "@chakra-ui/react";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { FaLinkedin, FaPhone, FaEnvelope } from "react-icons/fa";
 import { FiGithub } from "react-icons/fi";
 import logo from './byteburstlogo.jpg';
+import scribbleAnimation from "./scribble-animation.json";
 
 // Customizing the default theme
 const theme = extendTheme({
@@ -20,6 +39,16 @@ const theme = extendTheme({
 });
 
 function App() {
+  const [showCard, setShowCard] = useState(false);
+
+  const handlePhoneIconClick = () => {
+    setShowCard(true);
+  };
+
+  const handleCloseCard = () => {
+    setShowCard(false);
+  };
+
   return (
     <Box className="App">
       <Box className="header">
@@ -27,7 +56,7 @@ function App() {
           <Image src={logo} alt="Logo" className="logo-image" />
         </Box>
         <Box className="byteburst-container">
-          <p className="byteburst-text">
+          <p className="byteburst-text animate__animated animate__backInRight">
             <strong>ByteBurst</strong>
           </p>
         </Box>
@@ -40,11 +69,11 @@ function App() {
                 as={Button}
                 rightIcon={<ChevronDownIcon />}
                 variant="ghost"
-                color="whiteAlpha.900" // Update colorScheme to "whiteAlpha"
+                color="#7a060b"
                 _hover={{ bgColor: "blue.100" }}
                 _expanded={{ bgColor: "blue.200" }}
                 _focus={{ boxShadow: "outline" }}
-                mr={6} // Increase the spacing to 6
+                mr={6}
               >
                 Vision
               </MenuButton>
@@ -66,11 +95,11 @@ function App() {
                 as={Button}
                 rightIcon={<ChevronDownIcon />}
                 variant="ghost"
-                color="whiteAlpha.900" // Update colorScheme to "whiteAlpha"
+                color="#7a060b"
                 _hover={{ bgColor: "blue.100" }}
                 _expanded={{ bgColor: "blue.200" }}
                 _focus={{ boxShadow: "outline" }}
-                mr={6} // Increase the spacing to 6
+                mr={6}
               >
                 Our Projects
               </MenuButton>
@@ -92,11 +121,11 @@ function App() {
                 as={Button}
                 rightIcon={<ChevronDownIcon />}
                 variant="ghost"
-                color="whiteAlpha.900" // Update colorScheme to "whiteAlpha"
+                color="#7a060b"
                 _hover={{ bgColor: "blue.100" }}
                 _expanded={{ bgColor: "blue.200" }}
                 _focus={{ boxShadow: "outline" }}
-                mr={6} // Increase the spacing to 6
+                mr={6}
               >
                 Our Consultants
               </MenuButton>
@@ -118,11 +147,11 @@ function App() {
                 as={Button}
                 rightIcon={<ChevronDownIcon />}
                 variant="ghost"
-                color="whiteAlpha.900" // Update colorScheme to "whiteAlpha"
+                color="#7a060b"
                 _hover={{ bgColor: "blue.100" }}
                 _expanded={{ bgColor: "blue.200" }}
                 _focus={{ boxShadow: "outline" }}
-                mr={6} // Increase the spacing to 6
+                mr={6}
               >
                 Contact
               </MenuButton>
@@ -147,14 +176,72 @@ function App() {
           </Box>
         </Box>
       </Box>
-      <Box className="footer" pos="fixed" bottom="0" left="0" right="0" p="4" bg="#7a0606" color="white">
+      <Box
+        className="footer"
+        pos="fixed"
+        bottom="0"
+        left="0"
+        right="0"
+        p="4"
+        bg="#7a0606"
+        color="white"
+      >
         <ButtonGroup>
-          <Button leftIcon={<FaLinkedin />} variant="ghost" color="#d39b22" />
-          <Button leftIcon={<FaPhone />} variant="ghost" color="#d39b22" />
-          <Button leftIcon={<FaEnvelope />} variant="ghost" color="#d39b22" />
-          <Button leftIcon={<FiGithub />} variant="ghost" color="#d39b22" />
+          <Button
+            as="a"
+            href="https://www.linkedin.com/company/byteburst/?viewAsMember=true"
+            target="_blank"
+            rel="noopener noreferrer"
+            leftIcon={<FaLinkedin />}
+            variant="ghost"
+            color="#d39b22"
+          />
+          {/* Phone Icon - Card */}
+          <Button
+            leftIcon={<FaPhone />}
+            variant="ghost"
+            color="#d39b22"
+            onClick={handlePhoneIconClick}
+          />
+          <Button
+            leftIcon={<FaEnvelope />}
+            variant="ghost"
+            color="#d39b22"
+          />
+          <Button
+            as="a"
+            href="https://github.com/sebbeflebbe"
+            target="_blank"
+            rel="noopener noreferrer"
+            leftIcon={<FiGithub />}
+            variant="ghost"
+            color="#d39b22"
+          />
         </ButtonGroup>
       </Box>
+      {/* Card */}
+      {showCard && (
+        <Card maxW="md" mx="auto" mt={2} p={2}>
+          <CardHeader>Sebastian Andersson</CardHeader>
+          <CardBody>
+            <Image src="./self-portrait.jpg"/>
+            070-0393 805
+          </CardBody>
+          <Button onClick={handleCloseCard}>Close</Button>
+        </Card>
+      )}
+      {/* Lottie Animation */}
+      <div className="animation-container" style={{ position: 'fixed', top: '10px', right: '-180px' }}>
+  <Lottie
+    options={{
+      animationData: scribbleAnimation,
+      loop: true,
+      autoplay: true,
+    }}
+    width={600}
+    height={130}
+  />
+</div>
     </Box>
   );
 }
